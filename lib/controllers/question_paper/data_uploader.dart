1a45bb1dc880e6d166f947b28a193ce1758ffca8
twentiecker <twentiecker@gmail.com>
2023-03-12 14:29:04 +0700

    modify minSdkVersion to 21

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
+import '../../firebase_ref/references.dart';
 import '../../model/question_paper_model.dart';
 
 class DataUploader extends GetxController {
@@ -14,6 +16,8 @@ class DataUploader extends GetxController {
   }
 
   Future<void> uploadData() async {
+    /* create fireStore instance */
+    final fireStore = FirebaseFirestore.instance;
     /* loading all assets folder into 'AssetManifest.json' */
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
         .loadString('AssetManifest.json');
@@ -30,10 +34,35 @@ class DataUploader extends GetxController {
     /* read the content of the file that we get from paperInAssets */
     for (var paper in paperInAssets) {
       String stringPaperContent = await rootBundle.loadString(paper);
-      questionPapers.add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
       // print(stringPaperContent);
     }
     // print('Items number ${questionPapers.length}');
     // print(questionPapers[0].questions![2].id);
+    /* upload multiple data to firebase with batch operation */
+    var batch = fireStore.batch();
+    for (var paper in questionPapers) {
+      /* paper.id as name of collection (table) */
+      batch.set(questionPaperRF.doc(paper.id), {
+        /* set the key value of data */
+        "title": paper.title,
+        "image_url": paper.imageUrl,
+        "description": paper.description,
+        "time_seconds": paper.timeSeconds,
+        "questions_count": paper.questions == null ? 0 : paper.questions!.length
+      });
+      /* create questions to firebase as new collection and data */
+      for (var questions in paper.questions!) {
+        final questionPath =
+            questionRF(paperId: paper.id, questionId: questions.id);
+        batch.set(questionPath, {
+          "question": questions.question,
+          "correct_answer": questions.correctAnswer
+        });
+      }
+    }
+    /* submit batch into firebase database */
+    await batch.commit();
   }
 }
---
+++ lib/firebase_ref/references.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final fireStore = FirebaseFirestore.instance;
+/* create collection references,  *
+*  define the name of collection  *
+*  (question_papers) on the cloud *
+*  firestore rules.               */
+final questionPaperRF = fireStore.collection('question_papers');
+
+/* create reference method as DocumentReference */
+DocumentReference questionRF(
+        {
+        /* set parameters*/
+        required String paperId,
+        required String questionId}) =>
+    /* we looking for paperId as parent of the questions and then create *
+    *  collection name called "questions" with the neme of questionId.   */
+    questionPaperRF.doc(paperId).collection("questions").doc(questionId);
